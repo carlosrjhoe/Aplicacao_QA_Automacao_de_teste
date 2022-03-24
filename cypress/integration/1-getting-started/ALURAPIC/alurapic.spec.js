b384726dabd5a4822e955e6941bbed191d280921
Carlos Roberto <carlosrjhoe@gmail.com>
2022-03-24 19:34:17 -0300

    Interação com elemento HTML: Identificando elementos

---
+++ cypress/integration/1-getting-started/ALURAPIC/alurapic.spec.js
@@ -0,0 +1,15 @@
+describe('alura busca casos', () => {
+    beforeEach(() => {
+        cy.visit('https://alura-fotos.herokuapp.com');
+    })
+
+    it('Verifica mensagens validacao', () => {
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Email is required!').should('be.visible');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Full name is required!').should('be.visible'); // localizar um elemento e verificar algo que deva está visivel
+        cy.contains('ap-vmessage', 'User name is required!').should('be.visible');
+        cy.contains('ap-vmessage', 'Password is required!').should('be.visible');
+    })
+})
